729a28f1be41124972e299d24c418b8bb057f59a
Francois Lebel <francoislebel@gmail.com>
2014-03-13 18:42:30 -0400

    Nitpicks.

--- ip.py
+++ ip.py
@@ -5,7 +5,7 @@ from wsgiref.simple_server import make_server
 
 def get_ip(environ, start_response):
   status = '200 OK'
-  response_headers = [('Content-type','text/plain')]
+  response_headers = [('Content-type', 'text/plain')]
   start_response(status, response_headers)
   if 'HTTP_X_FORWARDED_FOR' in environ:
     return [environ['HTTP_X_FORWARDED_FOR'].split(',')[0]]
@@ -15,6 +15,6 @@ def get_ip(environ, start_response):
 
 if __name__ == '__main__':
   httpd = make_server('', 8000, get_ip)
-  print "Serving HTTP on port 8000..."
+  print 'Serving HTTP on port 8000...'
   httpd.serve_forever()
 
